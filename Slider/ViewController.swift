4c4b9c68e5a354667ca7f3f8c4ca913d0b5c912a
aerograf333 <rkhorenko@curiousit.eu>
2017-06-14 15:19:02 +0300

    Initial commit

--- Slider/ViewController.swift
+++ Slider/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var slayder: UISlider!
+    @IBAction func slayderChanged(_ sender: Any) {
+       table.reloadData()    }
+    @IBOutlet var table: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+
+    	}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    }
 
 }
 
+extension ViewController : UITableViewDelegate {
+
+}
+
+extension ViewController : UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 100000000//cellCotent.count
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String((Int(slayder.value * 20)) * (indexPath.row+1))
+        return cell
+    }
+}
